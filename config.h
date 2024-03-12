0b1ba058412e400a8234a51039bad6f0f39f3756
GreatBlastX <ghalem0chaouch@gmail.com>
2024-03-12 00:57:09 +0000

    Initial commit

--- config.h
+++ config.h
@@ -6,6 +6,7 @@ static const Block blocks[] = {
   {"",  "volume", 1,  10},
   {"",  "clock",  60, 1},
   {"",  "internet", 5,  4},
+  {"", "memory", 60, 4},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
